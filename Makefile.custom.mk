89c64dd31a6f5b647bed37327db1753c78b9d015
Matias Charriere <matias@giantswarm.io>
2025-07-23 11:15:58 +0200

    remove header form custom makefile
    Signed-off-by: Matias Charriere <matias@giantswarm.io>

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -1,25 +1,3 @@
-# DO NOT EDIT. Generated with:
-#
-#    devctl
-#
-#    https://github.com/giantswarm/devctl/blob/6a704f7e2a8b0f09e82b5bab88f17971af849711/pkg/gen/input/makefile/internal/file/Makefile.template
-#
-
-# include Makefile.*.mk (commented out as these files don't exist)
-
-##@ General
-
-# The help target prints out all targets with their descriptions organized
-# beneath their categories. The categories are represented by '##@' and the
-# target descriptions by '##'. The awk commands is responsible for reading the
-# entire set of makefiles included in this invocation, looking for lines of the
-# file as xyz: ## something, and then pretty-format the target and help. Then,
-# if there's a line with ##@ something, that gets pretty-printed as a category.
-# More info on the usage of ANSI control characters for terminal formatting:
-# https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_parameters
-# More info on the awk command:
-# http://linuxcommand.org/lc3_adv_awk.php
-
 ##@ Build
 
 .PHONY: build
